0f997a4583d959f27653a6c17afffb4ad60253bc
Tanmay09516 <tanmay5tj@gmail.com>
2022-11-05 14:56:18 +0530

    weeeeeeeeeeeeeeeee

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2022, 12, 15, 0)}); // year, month, date, hour
+        });
+});		
+
