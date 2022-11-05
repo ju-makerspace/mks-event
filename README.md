c34ecc50d5337475085db2139d47bfd817b0f17b
Tanmay09516 <tanmay5tj@gmail.com>
2022-11-05 14:56:00 +0530

    weeeeeeeeeee

--- README.md
+++
@@ -1,1 +0,0 @@
-# mks-event
--- js/countdown-custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022, 12, 15, 0)}); // year, month, date, hour
-        });
-});		
-
